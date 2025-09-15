cd4574fd7c2e0aa4b1aa11aba13381a5db08c85a
Alessandro Zunino <zuninoalessandro@gmail.com>
2025-09-15 14:05:28 +0200

    Minor fixes

--- demo/demo_sim.py
+++ demo/demo_sim.py
@@ -16,7 +16,7 @@ T = 200                # number of time points
 C = 9                 # number of channels
 dt = 0.01              # time step
 true_k = 0.1           # shared decay rate
-noise_level = 0.01     # additive noise
+noise_level = 0.00     # additive noise
 bkg_level = 0.10        # background strength
 
 time = torch.arange(T, dtype=torch.float32) * dt
@@ -28,11 +28,11 @@ torch.manual_seed(0)  # reproducibility
 irfs = torch.zeros(T, C)
 
 for c in range(C):
-    peak_idx_1 = torch.randint(T//2 - 10, T//2 + 10, (1,)).item()   # random peak location
+    peak_idx_1 = torch.randint(T//2 - 0, T//2 + 20, (1,)).item()   # random peak location
     sigma = 4
     t = torch.arange(T)
     irf = torch.exp(-0.5 * ((t - peak_idx_1)/sigma)**2)
-    peak_idx_2 = torch.randint(T//2 - 8, T//2 + 8, (1,)).item()   # random peak location
+    peak_idx_2 = peak_idx_1 + torch.randint(-15, 15, (1,)).item()   # random peak location
     irf +=0.7 * torch.exp(-0.5 * ((t - peak_idx_2)/sigma)**2)
     irf /= irf.sum()  # normalize
     irfs[:, c] = irf
@@ -71,25 +71,13 @@ retrieved_irf = b.run(lr=5e-2, steps=2000, rl_iterations=50)
 
 #%%
 
-b.plot_raw_and_fit()
+_ = b.plot_raw_and_fit()
 
-#%% --- Plot example channels ---
+#%%
 
-fig, ax = plt.subplots(3, 3, figsize=(12,10))
-ax = np.array(ax).reshape(-1)
+fig, ax = plot_dataset(b.time, irfs / irfs.max(dim = 0, keepdim=True).values)
 
-for c in range(C):
-    # ax[c].plot(time.numpy(), irfs[:, c].numpy(), '-', color='C0', label='true IRF')
-    ax[c].plot(time.numpy(), retrieved_irf[:, c].numpy(), '-', color='C1', label='Retrieved IRF')
-    ax[c].set_title(f'Channel {c}')
-    ax[c].set_xlabel('Time')
-    ax[c].set_ylabel('Intensity')
-
-for c in range(C, len(ax)):
-    ax[c].axis('off')
-
-fig.tight_layout()
-plt.show()
+fig, ax = plot_dataset(b.time, retrieved_irf / retrieved_irf.max(dim = 0, keepdim=True).values, fig = fig, ax = ax, color = 'C1', linestyle='dashed')
 
 #%%
 
